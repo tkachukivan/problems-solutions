6574980e58939544ed726293041760f61675c33a
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-04-07 23:40:55 +0200

    merge sorted arrays

---
+++ leetcode/easy/merge-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    let i = m - 1, j = n - 1, current = m + n - 1;
+    while(i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[current--] = nums1[i--];
+        } else {
+            nums1[current--] = nums2[j--];
+        }
+    }
+    
+    while(j >= 0) {
+        nums1[current--] = nums2[j--]; 
+    }
+};
