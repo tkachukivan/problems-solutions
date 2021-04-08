0110648a5a1f188abbc8f6ec48f12c9d68d642f1
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-04-08 22:05:22 +0200

    factorial trailing zeros

---
+++ leetcode/easy/factorial-trailing-zeroes.js
@@ -0,0 +1,15 @@
+/** https://leetcode.com/problems/factorial-trailing-zeroes/
+
+ * @param {number} n
+ * @return {number}
+ */
+ var trailingZeroes = function(n) {
+    if (n <= 4) return 0;
+    let zeros = 0;
+    while (n != 0) {
+        n = Math.floor(n / 5);
+        zeros += n;
+    }
+    
+    return zeros;
+};
