cbd49ba1a804a6319c23ebe684da7756480f9768
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-04-11 22:05:08 +0200

    middle of linked list, happy number two pointers

--- leetcode/easy/happy-number.js
+++ leetcode/easy/happy-number.js
@@ -17,4 +17,25 @@
         n = newN;
     }
     return true;
+    // two pointers
+    // let fast = n;
+    // let slow = n;
+    // while (fast !== 1) {
+    //     fast = happy(fast);
+    //     fast = happy(fast);
+    //     slow = happy(slow);
+    //     if (slow === fast) return slow === 1;
+    // }
+    
+    // return true;
+    
+    // function happy(n) {
+    //     let newN = 0;
+    //     while(n > 0) {
+    //         const num = n % 10;
+    //         newN += num * num;
+    //         n = Math.floor(n/10);
+    //     }
+    //     return newN;
+    // }
 };
