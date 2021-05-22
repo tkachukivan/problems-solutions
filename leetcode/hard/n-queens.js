289db0b6431f421fcc77096f601ca2b8cf4cf5bd
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-05-22 17:12:28 +0200

    n queens

--- leetcode/hard/n-queens.js
+++ leetcode/hard/n-queens.js
@@ -43,3 +43,48 @@
         return true;
     }
 };
+
+// var solveNQueens = function(n) {
+//     const board = Array.from({ length: n }, () => new Array(n).fill('.'));
+//     const result = [];
+//     collectBoards(0, board);
+    
+//     return result;
+    
+//     function collectBoards(row, board) {
+//         if (row === n) {
+//             result.push(board.map(row => row.join('')));
+//             return;
+//         }
+        
+//         for (let col = 0; col < n; col++) {
+//             if (isValid(board, row, col)) {
+//                 board[row][col] = 'Q';
+//                 collectBoards(row + 1, board);
+//                 board[row][col] = '.';
+//             }
+//         }
+//     }
+    
+//     function isValid(board, row, col) {
+//         // check from top to current
+//         for (let i = 0; i < row; i++) {
+//             if (board[i][col] === 'Q') return false;
+//         }
+        
+//         // check from current to left top
+//         let i = row - 1;
+//         let j = col - 1;
+//         while(i >= 0 && j >= 0) {
+//             if (board[i--][j--] === 'Q') return false;
+//         }
+//         // check from current to right top
+//         i = row - 1;
+//         j = col + 1;
+//         while(i >= 0 && j < n) {
+//             if (board[i--][j++] === 'Q') return false;
+//         }
+        
+//         return true;
+//     }
+// };
