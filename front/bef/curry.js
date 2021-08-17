b978ec4974fc2a2198a65720d5b7501ff0c2244e
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-08-17 23:01:08 +0200

    curry

---
+++ front/bef/curry.js
@@ -0,0 +1,15 @@
+/**
+ * @param { (...args: any[]) => any } fn
+ * @returns { (...args: any[]) => any }
+ */
+ function curry(fn) {
+    function inner(...args) {
+      if (args.length >= fn.length) {
+        return fn.apply(this, args);
+      } else {
+        return (...newArgs) => inner.apply(this, args.concat(newArgs));
+      }
+    }
+  
+    return inner;
+  }
