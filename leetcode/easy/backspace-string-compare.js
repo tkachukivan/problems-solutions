b4cfeec1129100491660bfa8c371e95168888a26
Ivan Tkachuk <tkachukivan93@gmail.com>
2021-10-19 17:46:23 +0200

    update solution

--- leetcode/easy/backspace-string-compare.js
+++ leetcode/easy/backspace-string-compare.js
@@ -4,25 +4,48 @@
  * @param {string} t
  * @return {boolean}
  */
- var backspaceCompare = function(s, t) {
-    let sEnd = s.length - 1;
-    let tEnd = t.length - 1;
-    let sBack = 0;
-    let tBack = 0;
-    while(sEnd >= 0 || tEnd >= 0) {
-        while (sEnd >= 0) { // Find position of next possible char in build(S)
-            if (s[sEnd] === '#') { sBack++; sEnd--; }
-            else if (sBack > 0) { sBack--; sEnd--; }
-            else break;
-        }
-        while (tEnd >= 0) { // Find position of next possible char in build(T)
-            if (t[tEnd] === '#') { tBack++; tEnd--; }
-            else if (tBack > 0) { tBack--; tEnd--; }
-            else break;
-        }
+//  var backspaceCompare = function(s, t) {
+//     let sEnd = s.length - 1;
+//     let tEnd = t.length - 1;
+//     let sBack = 0;
+//     let tBack = 0;
+//     while(sEnd >= 0 || tEnd >= 0) {
+//         while (sEnd >= 0) { // Find position of next possible char in build(S)
+//             if (s[sEnd] === '#') { sBack++; sEnd--; }
+//             else if (sBack > 0) { sBack--; sEnd--; }
+//             else break;
+//         }
+//         while (tEnd >= 0) { // Find position of next possible char in build(T)
+//             if (t[tEnd] === '#') { tBack++; tEnd--; }
+//             else if (tBack > 0) { tBack--; tEnd--; }
+//             else break;
+//         }
         
-        if (s[sEnd--] !== t[tEnd--]) return false;
+//         if (s[sEnd--] !== t[tEnd--]) return false;
+//     }
+    
+//     return true;
+// };
+const BACKSPACE = '#'
+// much simpler version
+function backspaceCompare(s1, s2) {
+    // Write your code here
+    s1 = removeBackspaces(s1);
+    s2 = removeBackspaces(s2);
+    
+    return s1 === s2 ? 1 : 0;
+}
+
+function removeBackspaces(str) {
+    const stack = [];
+    
+    for (const letter of str) {
+        if (letter === BACKSPACE) {
+            stack.pop();
+        } else {
+            stack.push(letter);
+        }
     }
     
-    return true;
-};
+    return stack.join('');
+}
